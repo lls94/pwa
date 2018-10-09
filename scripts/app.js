2cf3c8ebf1fb87f7db08580d04de784189fe8592
lilishi <1173967838@qq.com>
2018-10-09 22:33:43 +0800

    复制 pwa 源代码

---
+++ scripts/app.js
@@ -0,0 +1,152 @@
+const publicKey = 'BEvGz7_mk3I53v_hKzRwYorCKPpzvi1ZvFhnQ3TEHIemI65nzQMVghMCPI3-63V_arOQ_fdRiTWvBFrdNvEsJGY';
+
+function urlB64ToUint8Array(base64String) {
+    const padding = '='.repeat((4 - base64String.length % 4) % 4);
+    const base64 = (base64String + padding)
+        .replace(/\-/g, '+')
+        .replace(/_/g, '/');
+
+    const rawData = window.atob(base64);
+    const outputArray = new Uint8Array(rawData.length);
+
+    for (let i = 0; i < rawData.length; ++i) {
+        outputArray[i] = rawData.charCodeAt(i);
+    }
+    return outputArray;
+}
+
+
+
+function swRegister() {
+    navigator.serviceWorker.register("./sw.js", {
+        scope: "/"
+    }).then(function (reg) {
+        globalSwReg = reg;
+        if (checkIsSubscribed.bind(reg)()) {
+            console.log("订阅");
+        } else {
+            console.log("未订阅");
+        }
+        reg.update();
+        console.log("注册成功！");
+    }).catch(function () {
+        console.log("注册失败！");
+    })
+};
+
+function transJsonp() {
+    console.log(this, 20);
+    let appid = "20181007000216094";
+    let key = "TBtPAVPayrgJKdlRH6b_";
+    let salt = (new Date).getTime();
+    salt = 1;
+    let query = $(this).val().trim();
+    $.ajax({
+        type: 'get',
+        cache: true,
+        jsonpCallback: "cbFn",
+        dataType: 'jsonp',
+        url: "http://api.fanyi.baidu.com/api/trans/vip/translate",
+        data: {
+            appid: appid,
+            salt: salt,
+            sign: MD5(appid + query + salt + key),
+            q: query,
+            from: "auto",
+            to: "zh"
+        },
+        success: function (data) {
+            try {
+                let transResult = data.trans_result[0].dst;
+                $(".result>span").html(transResult);
+                console.log(transResult);
+            } catch (error) {
+                console.log("错了！");
+                $(".result>span").html('');
+            }
+
+        },
+        error: function (err) {
+            $(".result>span").html('查询失败！');
+        }
+    });
+}
+
+function addEvent() {
+    $(".input>textarea").blur(function () {
+        transJsonp.bind(this)();
+    });
+
+
+    $("button").click(function () {
+        notifyMe();
+    })
+}
+
+function checkIsSubscribed() {
+    globalSwReg.pushManager.getSubscription().then(function (subscription) {
+        if (subscription === null) {
+            return false;
+        } else {
+            return true;
+        }
+    })
+}
+
+
+$(() => {
+    if ("serviceWorker" in navigator && 'PushManager' in window) {
+        swRegister();
+    }
+    addEvent();
+});
+
+
+
+
+function notifyMe() {
+    console.log(103);
+
+    try {
+        globalSwReg.pushManager.subscribe({
+            userVisibleOnly: true,
+            applicationServerKey: urlB64ToUint8Array(publicKey)
+        }).then(function (subscription) {
+            console.log('User is subscribed:', subscription);
+            console.log(JSON.stringify(subscription), 116);
+            isSubscribed = true;
+        }).catch(function (err) {
+            console.log('Failed to subscribe the user: ', err);
+        });
+    } catch (error) {
+        console.log(error, 116)
+    }
+
+    console.log(120);
+
+    return;
+    // 先检查浏览器是否支持
+    if (!("Notification" in window)) {
+        alert("This browser does not support desktop notification");
+    }
+
+    // 检查用户是否同意接受通知
+    else if (Notification.permission === "granted") {
+        // If it's okay let's create a notification
+        var notification = new Notification("Hi there!");
+    }
+
+    // 否则我们需要向用户获取权限
+    else if (Notification.permission !== 'denied') {
+        Notification.requestPermission(function (permission) {
+            // 如果用户同意，就可以向他们发送通知
+            if (permission === "granted") {
+                var notification = new Notification("Hi there!");
+            }
+        });
+    }
+
+
+    // 最后，如果执行到这里，说明用户已经拒绝对相关通知进行授权
+    // 出于尊重，我们不应该再打扰他们了
+}
---
+++ sw.js
@@ -0,0 +1,94 @@
+'v1';
+self.addEventListener("install", function (event) {
+    event.waitUntil(
+        event.waitUntil(self.skipWaiting())
+    );
+});
+
+let cachesList = [
+    'res-cache-v1',
+    'fanyi-cache-v1'
+];
+
+self.addEventListener("activate", function (event) {
+    event.waitUntil(Promise.all([
+        self.clients.claim(),
+        caches.keys().then(function (cacheList) {
+            return Promise.all(
+                cacheList.map(function (cacheName) {
+                    if (cachesList.indexOf(cacheName) == -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        }),
+        caches.open(cachesList[0]).then(function (cache) {
+            console.log(4);
+            return cache.addAll([
+                '/',
+                'manifest.js',
+                'scripts/app.js',
+                'scripts/zepto.js',
+                'scripts/md5.js',
+                'style/index.css',
+                'images/change.png'
+            ])
+        })
+    ]))
+});
+
+self.addEventListener("fetch", function (event) {
+    event.respondWith(
+        caches.match(event.request).then(function (response) {
+
+            if (response) {
+                return response;
+            };
+
+            var request = event.request.clone();
+
+            return fetch(request).then(function (httpRes) {
+
+                let requestUrl = new URL(event.request.url);
+                let responseClone = httpRes.clone();
+
+                if (requestUrl.href.indexOf("api.fanyi.baidu.com/api/trans/vip/translate") != -1) {
+                    console.log(34);
+                    caches.open(cachesList[1]).then(function (cache) {
+                        cache.put(event.request, responseClone);
+                    });
+                } else if (!httpRes || httpRes.status !== 200) {
+                    return httpRes;
+                }
+
+                return httpRes;
+            })
+        })
+    )
+});
+
+self.addEventListener('push', function (event) {
+    console.log('[Service Worker] Push Received.');
+    console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
+
+    const title = 'Push Codelab';
+    const options = {
+        body: 'Yay it works.',
+        icon: 'images/icon.png',
+        badge: 'images/badge.png'
+    };
+
+    event.waitUntil(self.registration.showNotification(title, options));
+});
+
+
+
+self.addEventListener('notificationclick', function (event) {
+    console.log('[Service Worker] Notification click Received.');
+
+    event.notification.close();
+
+    event.waitUntil(
+        clients.openWindow('https://developers.google.com/web/')
+    );
+});
